aa14ba9d2adeabb3683a022f51cc3b0486be83eb
afhart12 <afhart@email.neit.edu>
2025-10-27 10:48:59 -0400

    added players

--- pong/pong/js/pong.js
+++ pong/pong/js/pong.js
@@ -28,9 +28,10 @@ ball.vx = -2
 ball.vy = -2
 ball.color = `black`
 
-let obj = new Player();
-obj.add("Player1");
-obj.add("Player2");
+var player = new Player();
+
+Player.add("Player 1", p1);
+Player.add("Player 2", p2);
 
 function main()
 {
